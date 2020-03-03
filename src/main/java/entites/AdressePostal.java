0a43caeedfdd9d94903724aa9872115e17f39911
Sébastien Marchand <sebastien.marchand78@gmail.com>
2020-03-03 14:18:50 +0100

    1r version TP 2 Instanciation et constructeur

--- src/main/java/entites/AdressePostal.java
+++ src/main/java/entites/AdressePostal.java
@@ -6,5 +6,17 @@ public class AdressePostal {
 	public String libelleDeLaRue;
 	public int codePostal;
 	public String ville;
+	
+	public AdressePostal()
+	{
+		
+	}
+	public AdressePostal(int numeroDeRue, String libelleDeLaRue, int codePostal, String ville)
+	{
+		this.numeroDeRue = numeroDeRue;
+		this.libelleDeLaRue = libelleDeLaRue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+	}
 
 }
--- src/main/java/entites/Personne.java
+++ src/main/java/entites/Personne.java
@@ -3,6 +3,21 @@ package entites;
 public class Personne {
 	public String nom;
 	public String prenom;
-	public String adressePostal;
+	public AdressePostal adressePostal = new AdressePostal();
+	
+	public Personne(String nom, String prenom)																			//Constructeur nom et prenom
+	{
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+	
+	
+	public Personne(String nom, String prenom, int numeroDeRue, String libelleDeLaRue, int codePostal, String ville)		//constructeur complet
+	{
+		this.nom = nom;
+		this.prenom = prenom;
+		this.adressePostal = new AdressePostal(numeroDeRue, libelleDeLaRue, codePostal, ville);
+		
+	}
 
 }
