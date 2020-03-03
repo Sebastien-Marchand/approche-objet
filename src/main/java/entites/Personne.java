93f3b4b4eb988f9e5339f617fa9c407b00497a56
Sébastien Marchand <sebastien.marchand78@gmail.com>
2020-03-03 16:32:52 +0100

    1e version TP 3 Instances et Méthodes

--- src/main/java/entites/Personne.java
+++ src/main/java/entites/Personne.java
@@ -1,9 +1,9 @@
 package entites;
 
 public class Personne {
-	public String nom;
-	public String prenom;
-	public AdressePostal adressePostal = new AdressePostal();
+	private String nom;
+	private String prenom;
+	private AdressePostal adressePostal = new AdressePostal();
 	
 	public Personne(String nom, String prenom)																			//Constructeur nom et prenom
 	{
@@ -18,6 +18,57 @@ public class Personne {
 		this.prenom = prenom;
 		this.adressePostal = new AdressePostal(numeroDeRue, libelleDeLaRue, codePostal, ville);
 		
+		
+	}
+	public String GetNom()
+	{
+		return this.nom.toUpperCase();
+	}
+	
+	public String GetPrenom()
+	{
+		return this.prenom;
+	}
+	public String GetAdresse() {
+		String monAdresse = this.adressePostal.numeroDeRue +" " + this.adressePostal.libelleDeLaRue +" "+this.adressePostal.codePostal+" "+ this.adressePostal.ville;
+		return monAdresse;
+	}
+	public boolean ChangerNom(String nom)
+	{
+		this.nom = nom;
+		if(this.nom == nom)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public boolean ChangerPrenom(String prenom)
+	{
+		this.prenom = prenom;
+		if(this.prenom == prenom)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	public boolean ChangerAdresse(int numeroDeRue, String libelleDeLaRue, int codePostal, String ville)
+	{
+		this.adressePostal = new AdressePostal(numeroDeRue, libelleDeLaRue, codePostal, ville);
+		if(this.prenom == prenom)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 
 }
--- src/main/java/essais/TestPersonne.java
+++ src/main/java/essais/TestPersonne.java
@@ -5,16 +5,18 @@ public class TestPersonne {
 
 	public static void main(String[] args) {
 		
-		Personne monPersonnage = new Personne("Malacoff", "joseph");
+	/*	Personne monPersonnage = new Personne("Malacoff", "joseph");
 		monPersonnage.adressePostal.numeroDeRue = 13;
 		monPersonnage.adressePostal.libelleDeLaRue = "rue de la poisse";
 		monPersonnage.adressePostal.codePostal = 62860;
-		monPersonnage.adressePostal.ville = "Marseille";
+		monPersonnage.adressePostal.ville = "Marseille";*/
 		
 		Personne monPersonnage2 = new Personne("paloudouli" , "Yen-gi" , 69 , "avenue du general du BBQ", 92850 ,"Bordeaux" );
 																											//Le nombre de ligne nécessaire a l'instanciation
-																							//est bien inférieur avec le constructeur complet
-		
+																											//est bien inférieur avec le constructeur complet
+		System.out.println("Monsieur :"+monPersonnage2.GetNom()+" "+monPersonnage2.GetPrenom()+ "\nvie a l'adresse : " + monPersonnage2.GetAdresse());
+		monPersonnage2.ChangerAdresse(45, "Rue des coquelicot", 58500, "Nantes");
+		System.out.println("Monsieur :"+monPersonnage2.GetNom()+" "+monPersonnage2.GetPrenom()+ "\nvie a l'adresse : " + monPersonnage2.GetAdresse());
 		
 	}
 
