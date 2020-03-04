696f15336cc6ff15a4ad0f8eb379feed73758c35
Sébastien Marchand <sebastien.marchand78@gmail.com>
2020-03-04 15:17:26 +0100

    1e version TP 3 Heritages et Constructeurs

--- src/main/java/fr/diginamic/banque/TestBanque.java
+++ src/main/java/fr/diginamic/banque/TestBanque.java
@@ -1,5 +1,6 @@
 package fr.diginamic.banque;
 import fr.diginamic.banque.entites.Compte;
+import fr.diginamic.banque.entites.CompteTaux;
 
 /**
  * @author atlantis
@@ -10,9 +11,19 @@ public class TestBanque {
 
 	public static void main(String[] args) {
 
-		Compte monCompte = new Compte(0, 1500);
+		Compte monCompte = new Compte(0, 2000);
 		System.out.println(monCompte);
+		CompteTaux monCompteTaux = new CompteTaux (0 , 1500 , 5);
+		System.out.println(monCompteTaux);
 
+		Compte monCompteTab[] = new Compte[2];
+		monCompteTab[0] = monCompte;
+		monCompteTab[1] = monCompteTaux;
+		
+		for(int i = 0 ; i < 2 ; i++)
+		{
+			System.out.println(monCompteTab[i]);
+		}
 	}
 
 }
---
+++ src/main/java/fr/diginamic/banque/entites/CompteTaux.java
@@ -0,0 +1,32 @@
+package fr.diginamic.banque.entites;
+
+public class CompteTaux extends Compte{
+
+	/** tauxRemuneration du compte*/
+	float tauxRemuneration = 0f;
+	
+	/**Constructeur par defaut
+	 * @param numeroCompte
+	 * @param soldeCompte
+	 */
+	public CompteTaux(int numeroCompte, int soldeCompte, float tauxRemuneration)
+	{
+		super(numeroCompte, soldeCompte);
+		this.tauxRemuneration = tauxRemuneration;
+	}
+	
+	/**
+	 *Redéfinition de la methode toString() de java.lang
+	 *Permet de retourner les information plutot que l'adresse de l'objet.
+	 */
+	@Override
+	
+	public String toString()
+	{
+		String information = "Numero du compte: " +numeroCompte +" Solde: "+ soldeCompte + "Taux de rémuneration du compte : "+tauxRemuneration;
+		return information;
+	}
+
+	
+	
+}
